0f43a2a5b0f5bf908f9209b9611193fe1c2f456c
grekova-web <71182152+grekova-web@users.noreply.github.com>
2021-11-22 15:27:02 +0500

    Мобильная версия для страницы преподавателей и фрагментов

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {Route} from "react-router-dom";
 import MainPage from "./components/MainPage/MainPage";
 import NavbarContainer from "./components/Navbar/NavbarContainer";
-import CatalogPage from "./components/CatalogPage/CatalogPage";
 import ProfilePageContainer from "./components/ProfilePage/ProfilePageContainer";
 import AdminAuthPageContainer from "./admin/AdminAuthPage/AdminAuthPageContainer";
 import ResetPasswordContainer from "./components/AuthPage/Login/ResetPassword/ResetPasswordContainer";
@@ -19,71 +18,67 @@ import TeachersContainer from "./components/Teachers/TeachersContainer";
 
 
 const App = () => {
-	return (
-		<>
-			<NavbarContainer/>
-			<Route
-				exact path='/'
-				render={() => <MainPage/>}
-			/>
-			<Route
-				exact path='/login'
-				render={() => <LoginContainer/>}
-			/>
-			<Route
-				exact path='/register'
-				render={() => <RegisterContainer/>}
-			/>
-			<Route
-				exact path='/catalog'
-				render={() => <CatalogPage/>}
-			/>
-			<Route
-				exact path='/catalog-fragments'
-				render={() => <FragmentsCatalog/>}
-			/>
-			<Route
-				exact path='/teachers'
-				render={() => <TeachersContainer/>}
-			/>
-			<Route
-				exact path='/my-fragments'
-				render={() => <MyFragments/>}
-			/>
-			<Route
-				exact path='/favorites'
-				render={() => <Favorite/>}
-			/>
-			<Route
-				exact path='/create-fragment'
-				render={() => <CreateFragmentContainer/>}
-			/>
-			<Route
-				exact path='/create-lesson'
-				render={() => <CreateLessonContainer/>}
-			/>
-			<Route
-				exact path='/profile/me'
-				render={() => <ProfilePageContainer/>}
-			/>
-			<Route
-				exact path='/profile:id?'
-				render={() => <TeacherProfileContainer/>}
-			/>
-			<Route
-				exact path='/admin/login'
-				render={() => <AdminAuthPageContainer/>}
-			/>
-			<Route
-				exact path='/reset-password:token?'
-				render={() => <ResetPasswordContainer/>}
-			/>
-			<Route
-				exact path='/fragment:id?'
-				render={() => <FragmentContainer/>}
-			/>
-		</>
-	)
+    return (
+        <>
+            <NavbarContainer/>
+            <Route
+                exact path='/'
+                render={() => <MainPage/>}
+            />
+            <Route
+                exact path='/login'
+                render={() => <LoginContainer/>}
+            />
+            <Route
+                exact path='/register'
+                render={() => <RegisterContainer/>}
+            />
+            <Route
+                exact path='/catalog-fragments'
+                render={() => <FragmentsCatalog/>}
+            />
+            <Route
+                exact path='/teachers'
+                render={() => <TeachersContainer/>}
+            />
+            <Route
+                exact path='/my-fragments'
+                render={() => <MyFragments/>}
+            />
+            <Route
+                exact path='/favorites'
+                render={() => <Favorite/>}
+            />
+            <Route
+                exact path='/create-fragment'
+                render={() => <CreateFragmentContainer/>}
+            />
+            <Route
+                exact path='/create-lesson'
+                render={() => <CreateLessonContainer/>}
+            />
+            <Route
+                exact path='/profile/me'
+                render={() => <ProfilePageContainer/>}
+            />
+            <Route
+                exact path='/profile:id?'
+                render={() => <TeacherProfileContainer/>}
+            />
+            <Route
+                exact path='/admin/login'
+                render={() => <AdminAuthPageContainer/>}
+            />
+            <Route
+                exact path='/reset-password:token?'
+                render={() => <ResetPasswordContainer/>}
+            />
+            <Route
+                exact path='/fragment:id?'
+                render={() => <FragmentContainer/>}
+            />
+        </>
+    )
 }
 
 export default App;
