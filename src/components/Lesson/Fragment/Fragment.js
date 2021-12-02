465eb02d0c5da6819fc9b79ac7448114772e2bec
Белов Артём <theflash.0320@gmail.com>
2021-12-02 21:25:38 +0500

    Теги на фрагменте в уроке

--- src/components/Lesson/Fragment/Fragment.js
+++ src/components/Lesson/Fragment/Fragment.js
@@ -10,17 +10,14 @@ const Fragment = ({fragment}) => {
 		<div className={s.fragment}>
 			<h3 className={s.title}>{fragment.title}</h3>
 			{fragment.type === fragmentTypes.article &&
-			<div className={s.article} dangerouslySetInnerHTML={{__html: fragment.content}}/>
+				<div className={s.article} dangerouslySetInnerHTML={{__html: fragment.content}}/>
 			}
 			{fragment.type === fragmentTypes.video &&
-			<video src={fragment.content} controls={'controls'} className={s.video}/>
+				<video src={fragment.content} controls={'controls'} className={s.video}/>
 			}
-			<div className={s.tagsAndAuthor}>
-				<div>Здесь будут теги фрагмента, когда Никита будет возвращать их с бэка</div>
-				<div className={s.author}>
-					<span className={s.authorName}>{fragment.user_name}</span>
-					<img src={fragment.avatar || avatarPlaceholder} alt="avatar" className={s.authorAvatar}/>
-				</div>
+			<div className={s.author}>
+				<span className={s.authorName}>{fragment.user_name}</span>
+				<img src={fragment.avatar || avatarPlaceholder} alt="avatar" className={s.authorAvatar}/>
 			</div>
 		</div>
 	);
