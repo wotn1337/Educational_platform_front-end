f1ee847df999573b8f493b2c86a85b4fea5ffa1b
grekova-web <71182152+grekova-web@users.noreply.github.com>
2021-12-07 14:51:22 +0500

    Адаптив карточки урока

--- src/components/CreateFragment/CreateFragment.js
+++ src/components/CreateFragment/CreateFragment.js
@@ -11,50 +11,50 @@ import ThisTags from "./ThisTags/ThisTags";
 
 
 const CreateFragment = (props) => {
-	const [showTagsList, setShowTagsList] = useState(false);
+    const [showTagsList, setShowTagsList] = useState(false);
 
-	return (
-		<div className={s.content}>
-			<h1 className={'pageTitle'}>Создать фрагмент</h1>
-			<SelectType/>
-			<FragmentTitle/>
-			<div style={{marginBottom: '20px'}}>
-				{props.fragmentType === fragmentTypes.article &&
-				<CreateArticle/>
-				}
-				{props.fragmentType === fragmentTypes.test &&
-				<CreateTestContainer/>
-				}
-				{props.fragmentType === fragmentTypes.video &&
-				<CreateVideo/>
-				}
-			</div>
-			{!!props.tags.length &&
-			<ThisTags
-				tags={props.tags}
-				edit={true}
-				returnTag={props.returnTag}
-				deleteTag={props.deleteTag}
-			/>
-			}
-			<div className={s.buttonsBlock}>
-				<button
-					className={`${s.btn} ${s.addTags}`}
-					onClick={() => setShowTagsList(!showTagsList)}
-				>
-					Добавить теги
-				</button>
-				<button
-					className={s.btn}
-					onClick={props.createFragment}
-					disabled={props.isFetching}
-				>
-					Создать
-				</button>
-			</div>
-			{showTagsList && <TagsListContainer currentTags={props.tags} externalAddTag={props.addTag}/>}
-		</div>
-	);
+    return (
+        <div className={s.content}>
+            <h1 className={'pageTitle'}>Создать фрагмент</h1>
+            <SelectType/>
+            <FragmentTitle/>
+            <div style={{marginBottom: '20px'}}>
+                {props.fragmentType === fragmentTypes.article &&
+                <CreateArticle/>
+                }
+                {props.fragmentType === fragmentTypes.test &&
+                <CreateTestContainer/>
+                }
+                {props.fragmentType === fragmentTypes.video &&
+                <CreateVideo/>
+                }
+            </div>
+            {!!props.tags.length &&
+            <ThisTags
+                tags={props.tags}
+                edit={true}
+                returnTag={props.returnTag}
+                deleteTag={props.deleteTag}
+            />
+            }
+            <div className={s.buttonsBlock}>
+                <button
+                    className={'btn'}
+                    onClick={() => setShowTagsList(!showTagsList)}
+                >
+                    Добавить теги
+                </button>
+                <button
+                    className={'btn'}
+                    onClick={props.createFragment}
+                    disabled={props.isFetching}
+                >
+                    Создать
+                </button>
+            </div>
+            {showTagsList && <TagsListContainer currentTags={props.tags} externalAddTag={props.addTag}/>}
+        </div>
+    );
 }
 
 export default CreateFragment;
