65f7563a7d883f50b1e96e365ea55b3e7f369691
grekova-web <71182152+grekova-web@users.noreply.github.com>
2021-12-19 11:41:39 +0500

    Доработка карточки урока

--- src/components/CatalogPage/lessonsCatalog/LessonsList/LessonCard/LessonCard.js
+++ src/components/CatalogPage/lessonsCatalog/LessonsList/LessonCard/LessonCard.js
@@ -8,7 +8,7 @@ import Tags from "../../../../MyFragments/FragmentsList/FragmentCard/Tags/Tags";
 const LessonCard = ({lesson, toggleFavorite, role, ...props}) => {
 	const [favorite, setFavorite] = useState(lesson.favourite);
 	return (
-		<div className={s.lessonCard}>
+		<NavLink to={`/lesson/${lesson.id}`} className={s.lessonCard}>
 			<div className={s.mainInfo}>
 				<img src={lessonPlaceholder} alt="lesson" className={s.preview}/>
 				<div className={s.tags}>
@@ -20,14 +20,15 @@ const LessonCard = ({lesson, toggleFavorite, role, ...props}) => {
 					<span className={s.fragmentsCountTitle}>Фрагментов в уроке: </span>
 					<span className={s.count}>{lesson.fragments_count}</span>
 				</span>
-				<NavLink to={`/lesson/${lesson.id}`} className={`${s.lessonButton} btn`}>
-					Перейти к материалам урока
-				</NavLink>
+				{/*<NavLink to={`/lesson/${lesson.id}`} className={`${s.lessonButton} btn`}>*/}
+				{/*	Перейти к материалам урока*/}
+				{/*</NavLink>*/}
 				{role !== 'admin' &&
 				<div className={s.favorite}>
 					<button
 						className={`${s.addToFavorite} ${favorite ? s.inFavorite : s.notInFavorite}`}
-						onClick={() => {
+						onClick={(event) => {
+							event.preventDefault();
 							toggleFavorite(lesson.id)
 								.then(() => setFavorite(!favorite));
 						}}
@@ -36,7 +37,7 @@ const LessonCard = ({lesson, toggleFavorite, role, ...props}) => {
 				}
 			</div>
 			<div className={s.author}>Автор: <NavLink to={`/profile/${lesson.user_id}`} className={s.creatorName}>{lesson.user_name}</NavLink></div>
-		</div>
+		</NavLink>
 	)
 		;
 };
