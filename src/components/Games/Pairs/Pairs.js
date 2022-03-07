9bc0f4bc169aa613651b5dfd844d9e3807e6557f
Белов Артём <artem.belov.0320@gmail.com>
2022-03-07 17:10:32 +0500

    feat(game_pairs): Добавлено модальное окно после завершения игры и анимация исчезновения найденных карт.

--- src/components/Games/Pairs/Pairs.js
+++ src/components/Games/Pairs/Pairs.js
@@ -1,14 +1,51 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import s from './Pairs.module.css';
 import {v1 as uuid} from "uuid";
 import {compose} from "redux";
 import {connect} from "react-redux";
+import {Transition} from "react-transition-group";
+import Modal from 'react-modal';
 
 
 const Pairs = ({images}) => {
 	const [inGame, setInGame] = useState(false)
 	const [cards, setCards] = useState(createCardsArray(images))
-	const [chosen, setChosen] = useState(undefined)
+	const [pair, setPair] = useState([])
+	const [pairCount, setPairCount] = useState(0)
+	const [openEndGameModal, setOpenEndGameModal] = useState(false)
+
+	// Проверка пары на соответствие
+	useEffect(() => {
+		if (pair.length === 2) {
+			const isPair = pair[0] === pair[1]
+			const index = pair[0]
+			if (isPair) {
+				setTimeout(() => setCards(cards.map(card => {
+					if (card.pairIndex === index) {
+						return {...card, finished: true}
+					}
+					return card
+				})), 700)
+				setPairCount(pairCount + 1)
+			} else {
+				setTimeout(() => setCards(cards.map(card => {
+					if (!card.finished) {
+						return {...card, rotated: true}
+					}
+					return card
+				})), 1000)
+			}
+			setPair([])
+		}
+	}, [pair])
+
+	useEffect(() => {
+		if (pairCount === images.length) {
+			setTimeout(() => {
+				setOpenEndGameModal(true)
+			}, 1500)
+		}
+	}, [pairCount])
 
 	const startGame = () => {
 		setInGame(true)
@@ -18,7 +55,9 @@ const Pairs = ({images}) => {
 	const restartGame = () => {
 		setInGame(false)
 		setCards(createCardsArray(images))
-		setChosen(undefined)
+		setPair([])
+		setPairCount(0)
+		setOpenEndGameModal(false)
 	}
 
 	const rotateAllCards = () => {
@@ -26,25 +65,18 @@ const Pairs = ({images}) => {
 	}
 
 	const rotateCardToFront = (targetCard) => {
-		const isChosen = chosen?.id === targetCard.id
-		if (!isChosen) {
-			setCards(cards.map(card => {
-				if (card.id === targetCard.id || (targetCard.pairIndex === chosen?.pairIndex && card.id === chosen?.id)) {
-					return {...card, rotated: false, finished: card.pairIndex === chosen?.pairIndex}
-				}
-				return card
-			}))
-		}
+		setCards(cards.map(card => {
+			if (card.id === targetCard.id) {
+				return {...card, rotated: false}
+			}
+			return card
+		}))
 	}
 
 	const onCardClick = (card) => {
-		if (inGame && !card.finished) {
-			if (!chosen) {
-				setChosen(card)
-			} else  {
-				setChosen(undefined)
-			}
+		if (inGame && !card.finished && card.rotated) {
 			rotateCardToFront(card)
+			setPair([...pair, card.pairIndex])
 		}
 	}
 
@@ -69,19 +101,46 @@ const Pairs = ({images}) => {
 					{inGame ? 'Начать заново' : 'Начать игру'}
 				</button>
 			</div>
+			<Modal
+				isOpen={openEndGameModal}
+				className={s.endGameModal}
+				overlayClassName={s.endGameOverlay}
+				onRequestClose={restartGame}
+			>
+				<div className={s.modalInner}>
+					<h3 className={s.modalHeader}>Отличная работа!</h3>
+					<div className={s.modalButtons}>
+						<button className={'btn'} onClick={restartGame}>Пройти еще раз</button>
+					</div>
+				</div>
+			</Modal>
 		</section>
 	);
 };
 
+// Состояния анимации для карточек
+const transitionStyles = {
+	entering: { transform: 'scale(0.5)' },
+	entered:  { opacity: 0 },
+	exiting:  { transform: 'scale(0.5)' },
+	exited:  { opacity: 1 },
+};
+
 const Card = ({image, rotated, rotateCard, finished}) => {
 	return (
-		<div
-			className={`${s.card} ${rotated ? s.rotated : ''}`}
-			onClick={rotateCard}
-		>
-			{finished && <div className={s.finished}/>}
-			<img className={s.cardImage} src={image} alt="card"/>
-		</div>
+		<Transition in={finished} timeout={500}>
+			{state => (
+				<div
+					className={`${s.card} ${rotated ? s.rotated : ''} ${finished ? s.finished : ''}`}
+					onClick={rotateCard}
+					style={{
+						...transitionStyles[state]
+					}}
+				>
+					<img className={s.cardImage} src={image} alt="card"/>
+				</div>
+			)}
+		</Transition>
 	)
 }
 
