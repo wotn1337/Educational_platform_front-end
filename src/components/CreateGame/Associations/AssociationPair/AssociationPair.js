26e91a562ef5521ac8e5f9bcde2b5bbada0c72d8
Grekova Elena <grekovalena01@gmail.com>
2022-04-06 20:04:07 +0500

    feat(ассоциации): создана компонента для ассоциативной пары

---
+++ src/components/CreateGame/Associations/AssociationPair/AssociationPair.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import s from './AssociationPair.module.css';
+import AssociationCard from "./AssociationCard/AssociationCard";
+import Tippy from "@tippyjs/react";
+
+const AssociationPair = ({setContent, setGameType, ...props}) => {
+    let cards=[];
+    for (let i=0; i<2; i++) {
+       cards.push(<AssociationCard key={i}
+                                   pairId={props.pairId}
+                                   imageId={i}
+                                   image={props.content[i]}
+                                   setContent={props.setContent}
+                                   setAssociation={props.setAssociation}/>)
+    }
+    return (
+        <Tippy
+            content={<button className={s.deletePair}
+                             onClick={event => {
+                                 event.preventDefault();
+                                 props.deleteAssociation(props.pairId)
+                             }}/>}
+            placement='right-start'
+            arrow={false}
+            animation='fade'
+            delay={[0, 50]}
+            interactive={true}
+        >
+            <section className={s.pairBlock} key={props.pairId}>
+                {cards}
+            </section>
+        </Tippy>
+    );
+};
+
+export default AssociationPair;
