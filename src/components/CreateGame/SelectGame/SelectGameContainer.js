240d7a768ca942bb73a3d4c69f57bc0fbf0465ff
Grekova Elena <grekovalena01@gmail.com>
2022-04-06 20:01:56 +0500

    fix(игры): добавлено поле "Задание" в конструкторе

--- src/components/CreateGame/SelectGame/SelectGameContainer.js
+++ src/components/CreateGame/SelectGame/SelectGameContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 import {connect} from "react-redux";
 import {compose} from "redux";
 import {getGames, setCurrentGame} from "../../../redux/gamesReducer";
-import {setContent, setGameType} from "../../../redux/createFragmentReducer";
+import {setContent, setGameType, setTask} from "../../../redux/createFragmentReducer";
 import SelectGame from "./SelectGame";
 
 class SelectGameContainer extends React.Component {
@@ -19,12 +19,15 @@ class SelectGameContainer extends React.Component {
 
 const mapStateToProps = (state) => ({
     games: state.games.games,
-    gameType: state.createFragment.gameType
+    task: state.createFragment.task,
+    gameType: state.createFragment.gameType,
+    isEdit: state.createFragment.isEdit
 })
 
 export default compose(connect(mapStateToProps, {
     setContent,
     setCurrentGame,
     setGameType,
+    setTask,
     getGames})
 )(SelectGameContainer)
